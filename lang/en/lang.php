13880123880f5d0da808da2639478af4f8bc2939
tiipiik <yohann.decharraud@gmail.com>
2014-08-07 15:57:10 +0200

    Added backend translations en / fr

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -2,6 +2,29 @@
 
 return [
     'backend'=> [
-        ''=>'',
+        'from_label'=>'From number',
+        'from_ph'=>'International number used to send SMS',
+        'from_comment'=>'Specify the international number for the "from" field of the message',
+        
+        'gateways_label'=>'Gateways',
+        'gateways_comment'=>'Select the gateway your using',
+        'gateways_ph'=>'-- Select your gateway --',
+        'clickatell_tab'=>'Clickatell',
+        
+        'clickatell_username'=>'Username',
+        'clickatell_username_ph'=>'Your Clickatell username',
+        'clickatell_username_comment'=>'The username you use for your Clickatell account',
+        
+        'clickatell_passwd'=>'Password',
+        'clickatell_passwd_ph'=>'Your Clickatell password',
+        'clickatell_passwd_comment'=>'The password you use for your Clickatell account',
+        
+        'clickatell_api_id'=>'API ID',
+        'clickatell_api_id_ph'=>'Your Clickatell Api Id',
+        'clickatell_api_id_comment'=>'API Id provided by Clickatell',
+        
+        'clickatell_api_url'=>'API URL',
+        'clickatell_api_url_ph'=>'Your Clickatell Api URL',
+        'clickatell_api_url_comment'=>'Modify this URL only if you know what you\'re doing',
     ]
 ];
---
+++ lang/fr/lang.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'backend'=> [
+        'from_label'=>'Numéro d\'envoi',
+        'from_ph'=>'Le numéro international pour les envois',
+        'from_comment'=>'Indiquez le numéro international utilisé pour envoyer les SMS',
+        
+        'gateways_label'=>'Passerelles',
+        'gateways_comment'=>'Sélectionnez la passerelle que vous utilisez',
+        'gateways_ph'=>'-- Sélectionnez votre passerelle --',
+        'clickatell_tab'=>'Clickatell',
+        
+        'clickatell_username'=>'Nom d\'utilisateur',
+        'clickatell_username_ph'=>'Votre nom d\'utilisateur Clickatell',
+        'clickatell_username_comment'=>'Le nom d\'utilisateur de votre compte Clickatell',
+        
+        'clickatell_passwd'=>'Mot de passe',
+        'clickatell_passwd_ph'=>'Votre mot de passe Clickatell',
+        'clickatell_passwd_comment'=>'Le mot de passe de votre compte Clickatell',
+        
+        'clickatell_api_id'=>'Identifiant API',
+        'clickatell_api_id_ph'=>'L\'identifiant de l\'API Clickatell que vous utilisez',
+        'clickatell_api_id_comment'=>'API Id provided by Clickatell',
+        
+        'clickatell_api_url'=>'Url API',
+        'clickatell_api_url_ph'=>'L\'url de l\'API Clickatell que vous utilisez',
+        'clickatell_api_url_comment'=>'Ne modifiez cette url que si vous savez ce que vous faites',
+    ]
+];
